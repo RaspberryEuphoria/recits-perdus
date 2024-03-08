371d4148fce8235dab3f9eec0c09f2e27fa1e2eb
RaspberryEuphoria <manon.dev.mion@gmail.com>
2024-03-08 13:11:04 +0100

    fix: change migration to work when there there's already data on the table

--- holocron/prisma/migrations/20240306212758_add_is_burned_column_to_dices_on_moves/migration.sql
+++ holocron/prisma/migrations/20240306212758_add_is_burned_column_to_dices_on_moves/migration.sql
@@ -5,4 +5,12 @@
 
 */
 -- AlterTable
-ALTER TABLE "DicesOnMoves" ADD COLUMN     "isBurned" BOOLEAN NOT NULL;
+
+/* Create a nullable column */
+ALTER TABLE "DicesOnMoves" ADD COLUMN     "isBurned" BOOLEAN;
+
+/* Set a default value for the previous entries in the table */
+UPDATE "DicesOnMoves" SET "isBurned" = false;
+
+/* Then set the column as non-nullable */
+ALTER TABLE "DicesOnMoves" ALTER COLUMN "isBurned" SET NOT NULL;
