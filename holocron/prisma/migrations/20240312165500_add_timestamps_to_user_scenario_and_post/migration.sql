2360f12be6c8bab046ef9b0ec9fa9f788b9253a1
RaspberryEuphoria <manon.dev.mion@gmail.com>
2024-03-13 10:05:52 +0100

    fix: add default values for last migration

--- holocron/prisma/migrations/20240312165500_add_timestamps_to_user_scenario_and_post/migration.sql
+++ holocron/prisma/migrations/20240312165500_add_timestamps_to_user_scenario_and_post/migration.sql
@@ -7,14 +7,33 @@
 
 */
 -- AlterTable
-ALTER TABLE "Post" ADD COLUMN     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-ADD COLUMN     "updatedAt" TIMESTAMP(3) NOT NULL;
+ALTER TABLE "Post" ADD COLUMN     "createdAt" TIMESTAMP(3) DEFAULT CURRENT_TIMESTAMP,
+ADD COLUMN     "updatedAt" TIMESTAMP(3);
+
+/* Set a default value for the previous entries in the table */
+UPDATE "Post" SET "updatedAt" = CURRENT_TIMESTAMP, "createdAt" = CURRENT_TIMESTAMP;
+
+ALTER TABLE "Post" ALTER COLUMN "updatedAt" SET NOT NULL;
+ALTER TABLE "Post" ALTER COLUMN "createdAt" SET NOT NULL;
 
 -- AlterTable
-ALTER TABLE "Scenario" ADD COLUMN     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-ADD COLUMN     "updatedAt" TIMESTAMP(3) NOT NULL;
+ALTER TABLE "Scenario" ADD COLUMN     "createdAt" TIMESTAMP(3) DEFAULT CURRENT_TIMESTAMP,
+ADD COLUMN     "updatedAt" TIMESTAMP(3);
+
+/* Set a default value for the previous entries in the table */
+UPDATE "Scenario" SET "updatedAt" = CURRENT_TIMESTAMP, "createdAt" = CURRENT_TIMESTAMP;
+
+ALTER TABLE "Scenario" ALTER COLUMN "updatedAt" SET NOT NULL;
+ALTER TABLE "Scenario" ALTER COLUMN "createdAt" SET NOT NULL;
 
 -- AlterTable
-ALTER TABLE "User" ADD COLUMN     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-ADD COLUMN     "lastActivityAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-ADD COLUMN     "updatedAt" TIMESTAMP(3) NOT NULL;
+ALTER TABLE "User" ADD COLUMN     "createdAt" TIMESTAMP(3) DEFAULT CURRENT_TIMESTAMP,
+ADD COLUMN     "lastActivityAt" TIMESTAMP(3) DEFAULT CURRENT_TIMESTAMP,
+ADD COLUMN     "updatedAt" TIMESTAMP(3);
+
+/* Set a default value for the previous entries in the table */
+UPDATE "User" SET "updatedAt" = CURRENT_TIMESTAMP, "lastActivityAt" = CURRENT_TIMESTAMP, "createdAt" = CURRENT_TIMESTAMP;
+
+ALTER TABLE "User" ALTER COLUMN "updatedAt" SET NOT NULL;
+ALTER TABLE "User" ALTER COLUMN "lastActivityAt" SET NOT NULL;
+ALTER TABLE "User" ALTER COLUMN "createdAt" SET NOT NULL;
