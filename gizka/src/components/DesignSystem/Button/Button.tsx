83518e5b2db6e529122c966bb224089e3792e65a
RaspberryEuphoria <manon.dev.mion@gmail.com>
2024-04-04 12:23:13 +0200

    feat(design system): add loader to Button

--- gizka/src/components/DesignSystem/Button/Button.tsx
+++ gizka/src/components/DesignSystem/Button/Button.tsx
@@ -1,3 +1,26 @@
+import LoadingIcon from '@/public/images/icons/loading.svg';
+
 import * as Styled from './styled';
 
-export const Button = Styled.Button;
+type ButtonProps = {
+  variant?: 'small';
+  width?: string;
+  outline?: boolean;
+  isLoading?: boolean;
+  disabled?: boolean;
+  onClick?: () => void;
+  children: React.ReactNode;
+};
+
+export function Button({ children, ...props }: ButtonProps) {
+  if (props.isLoading) {
+    return (
+      <Styled.Button {...props} isLoading>
+        <LoadingIcon />
+        {children}
+      </Styled.Button>
+    );
+  }
+
+  return <Styled.Button {...props}>{children}</Styled.Button>;
+}
--- gizka/src/components/DesignSystem/Button/styled.ts
+++ gizka/src/components/DesignSystem/Button/styled.ts
@@ -1,18 +1,31 @@
 import styled, { css } from 'styled-components';
 
-export const Button = styled.button<{ width?: string; outline?: boolean; variant?: 'small' }>`
+export const Button = styled.button<{
+  width?: string;
+  outline?: boolean;
+  variant?: 'small';
+  isLoading?: boolean;
+}>`
+  align-items: center;
   background: var(--flashy);
+  border-radius: var(--rounded);
   border: 2px solid;
   border-color: transparent;
-  border-radius: var(--rounded);
   color: var(--dark);
+  display: flex;
   font-family: 'Oxanium';
   font-size: 1.4rem;
+  gap: var(--space-1);
   outline: none;
   padding: var(--space-05) var(--space-2);
   text-transform: uppercase;
-  width: ${(props) => (props.width ? props.width : 'auto')};
   transition: background 0.3s ease-in;
+  width: ${(props) => (props.width ? props.width : 'auto')};
+
+  svg {
+    width: 25px;
+    height: 25px;
+  }
 
   ${(props) =>
     props.outline &&
@@ -25,16 +38,17 @@ export const Button = styled.button<{ width?: string; outline?: boolean; variant
   ${(props) =>
     props.variant === 'small' &&
     css`
-      align-items: center;
-      display: flex;
       font-size: 1.2rem;
       gap: var(--space-05);
       padding: var(--space-05) var(--space-1);
       text-transform: none;
+    `}
 
+${(props) =>
+    props.isLoading &&
+    css`
       svg {
-        width: 25px;
-        height: 25px;
+        animation: spin 1s linear infinite;
       }
     `}
 
@@ -55,6 +69,12 @@ export const Button = styled.button<{ width?: string; outline?: boolean; variant
   @keyframes pulse {
     0% {
       box-shadow: 0 0 0 0 var(--flashy);
+    }
+  }
+
+  @keyframes spin {
+    100% {
+      transform: rotate(360deg);
     }
   }
 `;
