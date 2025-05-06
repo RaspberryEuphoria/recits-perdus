dfc89fc3be047f4b00ecd2778270fa739c4811da
RaspberryEuphoria <manon.dev.mion@gmail.com>
2025-05-06 22:15:00 +0200

    refactor(web): remove unecessary code from ArchiveWithIdPage component

--- gizka/src/components/Pages/ArchiveWithIdPage/ArchiveWithIdPage.tsx
+++ gizka/src/components/Pages/ArchiveWithIdPage/ArchiveWithIdPage.tsx
@@ -1,26 +1,19 @@
 'use client';
 
 import { useTranslations } from 'next-intl';
-import { useEffect, useMemo, useRef, useState } from 'react';
-import { io, Socket } from 'socket.io-client';
+import { useMemo, useRef, useState } from 'react';
 
 import { CharacterList } from '@/components/CharacterList';
-import { DialogTextarea } from '@/components/Dialog/DialogTextarea';
-import { Move } from '@/components/Dialog/DialogTextarea/DialogTextarea';
 import { DialogThread } from '@/components/Dialog/DialogThread';
 import { LayoutAsideSection, LayoutMainSection } from '@/components/Layout';
-import { Moves } from '@/components/Moves';
 import { ScenarioResources } from '@/components/ScenarioResources';
 import DownArrowIcon from '@/public/images/icons/down_arrow.svg';
-import { httpBffClient, isHttpError } from '@/services/http-client';
 import { useLocalStorage } from '@/utils/hooks/localStorage';
 import { Character } from '@/utils/types/character';
-import { Post, Scenario } from '@/utils/types/scenario';
+import { Post } from '@/utils/types/scenario';
 import { User } from '@/utils/types/user';
 
 import * as Styled from './styled';
-
-const MAX_LENGTH = 1000;
 
 type ArchiveWithIdPageProps = {
   id: string;
@@ -32,27 +25,15 @@ type ArchiveWithIdPageProps = {
   supplies: number;
 };
 
-function mapScenarioCharacters(scenarioCharacters: Character[]) {
-  return scenarioCharacters.reduce((acc, character) => {
-    acc[character.id] = character;
-    return acc;
-  }, {} as Record<string, Character>);
-}
-
 enum Tab {
   Status = 'status',
-  Posting = 'posting',
   Reading = 'reading',
 }
 
-let socket: Socket;
-
 export function ArchiveWithIdPage({
-  id,
   title,
   introduction,
   posts: initialDialogs,
-  nextPoster: initialNextPoster,
   characters: initalCharacters,
   supplies: initalSupplies,
 }: ArchiveWithIdPageProps) {
@@ -60,107 +41,15 @@ export function ArchiveWithIdPage({
   const threadRef = useRef<HTMLDivElement>(null);
   const [currentUser] = useLocalStorage<User>('currentUser');
   const [openTabId, setOpenTabId] = useState<Tab>(Tab.Status);
-  const [characters, setCharacters] = useState<Record<string, Character>>(initalCharacters);
-  const [nextPoster, setNextPoster] = useState<Character>(initialNextPoster);
-  const [supplies, setSupplies] = useState<number>(initalSupplies);
-  const [dialogs, setDialogs] = useState<Post[]>(initialDialogs);
-  const [content, setContent] = useState<string>('');
-  const [postId, setPostId] = useState<number | null>(null);
+  const [characters] = useState<Record<string, Character>>(initalCharacters);
+  const [supplies] = useState<number>(initalSupplies);
+  const [dialogs] = useState<Post[]>(initialDialogs);
 
-  const isItMyTurn = currentUser ? currentUser.id === nextPoster.userId : false;
-  const isEditAllowed = openTabId === Tab.Status;
-  const showTextarea = currentUser && openTabId === Tab.Posting && (isItMyTurn || postId);
   const displayMainSection = openTabId !== Tab.Reading;
-
-  const handleContentChange = (newContent: string) => {
-    setContent(newContent);
-  };
-
-  const handleOpenTextarea = () => {
-    if (isItMyTurn) setOpenTabId(Tab.Posting);
-  };
-
-  const handlePostEdit = ({ id, content }: { id: number; content: string }) => {
-    setPostId(id);
-    setContent(content);
-    setOpenTabId(Tab.Posting);
-  };
-
-  const handleTextareaSubmit = () => {
-    setContent('');
-    setPostId(null);
-    setOpenTabId(Tab.Status);
-  };
-
-  const placeholder = isItMyTurn
-    ? "C'est à vous de jouer"
-    : `C'est au tour de ${nextPoster.firstName} ${nextPoster.lastName}`;
 
   const scrollToBottom = () => {
     if (threadRef.current) threadRef.current.scrollIntoView({ behavior: 'smooth', block: 'end' });
   };
-
-  const socketInitializer = async () => {
-    await httpBffClient.get('/init-socket');
-
-    socket = io(`${process.env.NEXT_PUBLIC_BFF_PREFIX_URL}`, {
-      path: '/api/socket',
-      transports: ['polling'],
-      withCredentials: true,
-    });
-
-    socket.on('receive-new-move', async () => {
-      const scenario = await httpBffClient.get<Scenario>(`/scenario/${id}`);
-
-      if (isHttpError(scenario)) {
-        throw new Error(
-          `There was an error while fetching the scenario ${id}: ${scenario.message}`,
-        );
-      }
-
-      setSupplies(scenario.supplies);
-      setCharacters(mapScenarioCharacters(scenario.characters));
-    });
-
-    socket.on('receive-new-dialog', async (newDialog) => {
-      const [scenarioId] = id.split('-');
-      if (scenarioId != newDialog.scenarioId) return;
-
-      setDialogs((dialogs) => [...dialogs, newDialog]);
-
-      if (newDialog.nextPoster) setNextPoster(newDialog.nextPoster);
-      if (newDialog.move) socket.emit('post-new-move');
-
-      socket.off('receive-new-dialog');
-    });
-
-    socket.on('receive-edited-dialog', async (updatedDialog) => {
-      const [scenarioId] = id.split('-');
-      if (scenarioId != updatedDialog.scenarioId) return;
-
-      setDialogs((dialogs) => {
-        const index = dialogs.findIndex((dialog) => dialog.id === updatedDialog.id);
-        if (index === -1) return dialogs;
-
-        const newDialogs = [...dialogs];
-        newDialogs[index] = updatedDialog;
-
-        return newDialogs;
-      });
-
-      if (updatedDialog.nextPoster) setNextPoster(updatedDialog.nextPoster);
-
-      socket.off('receive-edited-dialog');
-    });
-  };
-
-  useEffect(() => {
-    socketInitializer();
-
-    return () => {
-      if (socket) socket.disconnect();
-    };
-  }, []);
 
   const tabs = useMemo(() => {
     return [
@@ -196,28 +85,6 @@ export function ArchiveWithIdPage({
               <ScenarioResources supplies={supplies} />
             </CharacterList>
           )}
-          {showTextarea && (
-            <DialogTextarea
-              scenarioId={id}
-              maxLength={MAX_LENGTH}
-              nextPoster={nextPoster}
-              content={content}
-              onContentChange={handleContentChange}
-              onTextareaSubmit={handleTextareaSubmit}
-              postId={postId}
-              renderMoves={(
-                onMovePicked: (move: Move | null) => void,
-                onBurnCheck: (hasMomentumBurn: boolean) => void,
-              ) => (
-                <Moves
-                  onMovePicked={onMovePicked}
-                  onBurnCheck={onBurnCheck}
-                  character={nextPoster}
-                  characters={Object.values(characters)}
-                />
-              )}
-            />
-          )}
         </LayoutMainSection>
       )}
       <LayoutAsideSection
@@ -237,8 +104,9 @@ export function ArchiveWithIdPage({
           characters={characters}
           dialogs={dialogs}
           introductionText={introduction}
-          handlePostEdit={handlePostEdit}
-          isEditAllowed={isEditAllowed}
+          // eslint-disable-next-line @typescript-eslint/no-empty-function
+          handlePostEdit={() => {}}
+          isEditAllowed={false}
           ref={threadRef}
         />
       </LayoutAsideSection>
