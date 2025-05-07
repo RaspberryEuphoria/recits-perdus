4d1583ea4e097657d25169f6bc8468759c54b7c4
RaspberryEuphoria <manon.dev.mion@gmail.com>
2025-05-07 17:29:50 +0200

    feat(ui): add fallback to default avatar

--- gizka/src/components/CharacterAvatar/CharacterAvatar.tsx
+++ gizka/src/components/CharacterAvatar/CharacterAvatar.tsx
@@ -1,6 +1,7 @@
 import Image from 'next/image';
-import React from 'react';
+import React, { useState } from 'react';
 
+import DefaultAvatarSrc from '@/public/images/default_avatar.png';
 import { TextColor } from '@/utils/constants';
 import { convertHexadecimalColorToHsl } from '@/utils/scenario/helpers';
 import { Character } from '@/utils/types/character';
@@ -17,18 +18,23 @@ export function CharacterAvatar({
   character: { textColor, avatar, firstName },
   handleClick,
 }: CharacterAvatarProps) {
+  const [avatarSrc, setAvatarSrc] = useState<string>(
+    `${process.env.NEXT_PUBLIC_IMAGES_PREFIX_URL}/users/avatars/${avatar}`,
+  );
+
   return (
     <Styled.CharacterAvatar
-      colorAtLightOpacity={convertHexadecimalColorToHsl(textColor, 0.2)}
+      colorAtLightOpacity={convertHexadecimalColorToHsl(textColor || TextColor.Default, 0.2)}
       color={textColor || TextColor.Default}
       onClick={handleClick}
     >
       <Image
-        src={`${process.env.NEXT_PUBLIC_IMAGES_PREFIX_URL}/users/avatars/${avatar}`}
-        alt="Avatar"
+        src={avatarSrc}
+        alt={`Avatar de ${firstName}`}
         width={200}
         height={230}
         quality={100}
+        onError={() => setAvatarSrc(DefaultAvatarSrc.src)}
       />
       <Styled.CharacterName>{firstName}</Styled.CharacterName>
     </Styled.CharacterAvatar>
