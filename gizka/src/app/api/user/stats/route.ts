d11386ad52da669db485c07e166454a21a6325e4
gauvainboiche <gauvain.boiche+git@protonmail.com>
2025-06-06 17:13:14 +0200

    Dockerfiles and docker-compose #5

--- gizka/src/app/api/user/stats/route.ts
+++ gizka/src/app/api/user/stats/route.ts
@@ -4,6 +4,15 @@ import { httpClient } from '@/services/http-client';
 import { UserStats } from '@/utils/types/user';
 
 export async function GET() {
-  const data = await httpClient.get<UserStats>('/user/stats');
-  return NextResponse.json(data);
+  try {
+    const data = await httpClient.get<UserStats>('/user/stats');
+
+    return NextResponse.json(data);
+  } catch(err) {
+    console.log("Big Error HERE !!!")
+    console.log(err);
+  }
 }
+
+  // const response = await fetch(process.env.API_PREFIX_URL + '/user/stats');
+  // const data = await response.json();
