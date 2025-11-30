fc2598c3da8668399aeac1f3e0cf6644aab521f9
RaspberryEuphoria <manon.dev.mion@gmail.com>
2025-11-30 19:03:26 +0100

    fix: let post illustration take full width

--- gizka/src/components/Dialog/DialogPost/styled.ts
+++ gizka/src/components/Dialog/DialogPost/styled.ts
@@ -53,7 +53,7 @@ export const DialogIllustrationContainer = styled.div`
   border: 1px solid var(--flashy);
   border-radius: var(--rounded);
   box-shadow: 0 0 10px var(--flashy), inset 0 0 10px var(--flashy);
-  margin: 0 auto;
+  margin: 0;
   max-width: 886px;
   position: relative;
 
